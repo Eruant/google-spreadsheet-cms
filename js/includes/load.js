a7f439cfca6d9a1d7c3a184d8f6cd8628f7493c0
Matt Gale <matt@littleball.co.uk>
2013-09-13 17:06:21 +0100

    Adding rendering to gadgets

--- js/includes/load.js
+++ js/includes/load.js
@@ -3,144 +3,165 @@
 require(['Player', 'Advert'], function (Player, Advert) {
     'use strict';
     
-    var app = {
-        
-        rendered: {
+    var global = {
+            Player: Player,
+            Advert: Advert
         },
-        
-        init: function () {
+        app = {
             
-            var me = this;
+            // store the rendered output
+            rendered: {
+            },
             
-            Tabletop.init({
-                key: '0Ah2ptCYiQhr8dHVOb2J1aldZOFJPa0tjVXVZNWVnUmc',
-                simpleSheet: false,
-                parseNumbers: true,
-                callback: function (data) {
-                    me.data = data;
+            // store the code to exicute
+            codeStore: [],
+            
+            init: function () {
+                
+                var me = this;
+                
+                Tabletop.init({
+                    key: '0Ah2ptCYiQhr8dHVOb2J1aldZOFJPa0tjVXVZNWVnUmc',
+                    simpleSheet: false,
+                    parseNumbers: true,
+                    callback: function (data) {
+                        me.data = data;
+                        me.locationChange();
+                        me.setEvents();
+                    }
+                });
+            },
+            
+            setEvents: function () {
+                
+                var me = this;
+                
+                window.addEventListener('hashchange', function (ev) {
                     me.locationChange();
-                    me.setEvents();
+                }, false);
+            },
+            
+            render: function (data) {
+                
+                var	template, slots, len, item, i, slot;
+                
+                // check to see if there is already a rendered version of the page
+                if (this.rendered[data.url]) {
+                    template = this.rendered[data.url];
+                } else {
+                    
+                    // element to hold new data
+                    template = document.createElement('div');
+                    
+                    // add the template data
+                    template.innerHTML = document.getElementById(data.template).innerHTML;
+                    
+                    // find the slots
+                    slots = template.querySelectorAll('.col');
+                    
+                    len = slots.length;
+                    
+                    for (i = 0; i < len; i += 1) {
+                        item = slots[i];
+                        slot = data["slot" + i];
+                        
+                        if (slot) {
+                            if (slot === 'Navigation') {
+                                item.innerHTML = this.buildNavigation(data.url);
+                            } else {
+                                // replace this with gadget
+                                item.innerHTML = this.loadGadget(slot, data["slot" + i + "-data"]);
+                            }
+                        }
+                    }
+                    
+                    this.rendered[data.url] = template; // save the output
                 }
-            });
-        },
-        
-        setEvents: function () {
-            
-            var me = this;
-            
-            window.addEventListener('hashchange', function (ev) {
-                me.locationChange();
-            }, false);
-        },
-        
-        render: function (data) {
-            
-            var	template, slots, len, item, i, slot;
-            
-            // check to see if there is already a rendered version of the page
-            if (this.rendered[data.url]) {
-                template = this.rendered[data.url];
-            } else {
-            
-                // element to hold new data
-                template = document.createElement('div');
                 
-                // add the template data
-                template.innerHTML = document.getElementById(data.template).innerHTML;
+                // replace the template data
+                document.getElementById('template').innerHTML = template.innerHTML;
                 
-                // find the slots
-                slots = template.querySelectorAll('.col');
+                this.initGadgets();
+            },
+            
+            initGadgets: function () {
                 
-                len = slots.length;
+                var items = document.querySelectorAll('.gadget'),
+                    len = items.length,
+                    i,
+                    id,
+                    gadget,
+                    item;
                 
                 for (i = 0; i < len; i += 1) {
-                    item = slots[i];
-                    slot = data["slot" + i];
-                    
-                    if (slot) {
-                        if (slot === 'Navigation') {
-                            item.innerHTML = this.buildNavigation(data.url);
+                    item = this.codeStore[items[i].id];
+                    gadget = new global[item.gadget](item.id, item.options);
+                }
+            },
+            
+            loadGadget: function (sheet, data) {
+                
+                var dataset = this.data[sheet].elements,
+                    output;
+                
+                if (data === 'all') {
+                    output = sheet + ': Get all results ';
+                } else if (typeof data === 'number') {
+                    output = '<div id="' + dataset[data].id + '" class="gadget"></div>';
+                    this.codeStore[dataset[data].id] = { gadget: sheet, id: dataset[data].id, options: { videoId: dataset[data].videoid }};
+                } else {
+                    output = sheet + ': do the default action';
+                }
+                
+                return output;
+            },
+            
+            buildNavigation: function (currentUrl) {
+                
+                var len, item, i, nav, data;
+                
+                data = this.data.config.elements;
+                
+                nav = ['<ul class="nav">'];
+                
+                len = data.length;
+                for (i = 0; i < len; i += 1) {
+                    item = data[i];
+                    if (item.published === 1) {
+                        if (item.url === currentUrl) {
+                            nav.push('<li>' + item.title + '</li>');
                         } else {
-                            // replace this with gadget
-                            item.innerHTML = this.loadGadget(slot, data["slot" + i + "-data"]);
+                            nav.push('<li><a href="#' + item.url + '">' + item.title + '</a></li>');
                         }
                     }
                 }
                 
-                this.rendered[data.url] = template; // save the outpu
-            }
-            
-            // replace the template data
-            document.getElementById('template').innerHTML = template.innerHTML;
-        },
-        
-        loadGadget: function (sheet, data) {
-            
-            var dataset = this.data[sheet].elements,
-                output;
-
-            if (data === 'all') {
-                output = sheet + ': Get all results ';
-            } else if (typeof data === 'number') {
+                nav.push('</ul>');
                 
+                return nav.join('');
+            },
+            
+            locationChange: function () {
+                var cfg = this.data.config.elements,
+                    path = window.location.hash.replace(/^\#\/?/, '/').replace(/\/$/, ''),
+                    items,
+                    item,
+                    len,
+                    i;
                 
-                output = sheet + ': Get number ' + data;
-                if (dataset[data]) {
-                	output += '<br/>' + dataset[data].videoid;
-                }
-            } else {
-                output = sheet + ': do the default action';
-            }
-            
-            return output;
-        },
-        
-        buildNavigation: function (currentUrl) {
-            
-            var len, item, i, nav, data;
-            
-            data = this.data.config.elements;
-            
-            nav = ['<ul class="nav">'];
-                            
-            len = data.length;
-            for (i = 0; i < len; i += 1) {
-                item = data[i];
-                if (item.published === 1) {
-                    if (item.url === currentUrl) {
-                        nav.push('<li>' + item.title + '</li>');
-                    } else {
-                        nav.push('<li><a href="#' + item.url + '">' + item.title + '</a></li>');
+                items = cfg;
+                len = items.length;
+                for (i = 0; i < len; i += 1) {
+                    item = items[i];
+                    if (path === '' || (item.url === path && item.published === 1)) {
+                        this.render(item);
+                        return;
                     }
                 }
+                
+                this.render(cfg[0]); // location does not match show homepage
             }
-            
-            nav.push('</ul>');
-            
-            return nav.join('');
-        },
-        
-        locationChange: function () {
-            var cfg = this.data.config.elements,
-                path = window.location.hash.replace(/^\#\/?/, '/').replace(/\/$/, ''),
-                items,
-                item,
-                len,
-                i;
-            
-            items = cfg;
-            len = items.length;
-            for (i = 0; i < len; i += 1) {
-                item = items[i];
-                if (path === '' || (item.url === path && item.published === 1)) {
-                    this.render(item);
-                    return;
-                }
-            }
-            
-            this.render(cfg[0]); // location does not match show homepage
-        }
-    };
+        };
     
     app.init();
     
